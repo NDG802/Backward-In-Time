0e444540f22312b67607b5e1ca0ef96d37a9f954
Nicholas Griffey <nicholasgriffey@gmail.com>
2015-02-17 20:47:48 -0500

    Modify to use 'datetime' module, renamed functions and variables

--- time_fun.py
+++ time_fun.py
@@ -1,44 +1,87 @@
+from datetime import date, timedelta
 from math import floor, e, log
 from time import sleep, strftime
 
 
-class Date(object):
-    months = {"January": 0, "February": 31, "March": 59,
-              "April": 90, "May": 120, "June": 151,
-              "July": 181, "August": 212, "September": 243,
-              "October": 273, "November": 304, "December": 334}
-
+class Task(object):
     def __init__(self):
-        # Start date
-        day_of_year = int(strftime("%j"))
-        year = int(strftime("%Y"))
-        total_current_days = Date.number_of_days(year)
-        self.current_date = year + day_of_year / total_current_days
+        self.get_start_date()
+        self.get_stop_date()
+        self.difference_in_dates = (self.start_date - self.stop_date).days
 
-        # Stop date
-        year_stop = Date.stop_date_input("year", int)
-        month_stop = Date.stop_date_input("month", str)
-        day_stop = Date.stop_date_input("day", int)
-        total_stop_days = Date.number_of_days(year_stop)
-        if Date.months[month_stop] >= 59 and total_stop_days == 366:
-            day_stop += 1
-        self.stop_date = (year_stop +
-                          (Date.months[month_stop]+day_stop) / total_stop_days)
+        self.get_task_length()
 
-        # Length of task
-        hours = Date.get_number_of_seconds("hours", 2)
-        minutes = Date.get_number_of_seconds("minutes", 1)
-        seconds = Date.get_number_of_seconds("seconds", 0)
-        self.length = hours + minutes + seconds
+    def get_start_date(self):
+        self.start_date = date.today()
+        days_in_start_year = Task.number_of_days(self.start_date.year)
+        days_since_new_year = ((self.start_date -
+                               date(self.start_date.year, 1, 1)).days)
+        self.start_date_decimal = (self.start_date.year +
+                                   (days_since_new_year/days_in_start_year))
+
+    def get_stop_date(self):
+        self.stop_date = self.start_date + timedelta(days=1)
+        while self.stop_date > self.start_date:
+            try:
+                self.stop_date = (date(self.stop_date_input("year"),
+                                       self.stop_date_input("month"),
+                                       self.stop_date_input("day")))
+                if self.stop_date > self.start_date:
+                    print("Invalid input: date is in the future.")
+            except ValueError:
+                print("Invalid input: one or more values out of range.")
+
+    @staticmethod
+    def stop_date_input(datetype):
+        while True:
+            try:
+                value = (int(input("Enter the " + datetype +
+                         " you wish to stop in: ")))
+            except (ValueError):
+                print("Invalid input: date must be a number.")
+            else:
+                return value
+
+    def get_task_length(self):
+        nums = []
+        exponent = 2
+        for t_unit in ["hours", "minutes", "seconds"]:
+            while True:
+                try:
+                    seconds = (int(input("Enter length of the task in " +
+                               t_unit + ": ")))
+                    assert seconds % 1 == 0
+                except (ValueError, AssertionError):
+                    print("Invalid input: input must be an integer.")
+                else:
+                    nums.append(seconds * 60**exponent)
+                    exponent -= 1
+                    break
+        self.task_length = sum(nums)
 
     def progress(self):
-        self.exponent = log(self.current_date-self.stop_date+e**3) - 3
-        for i in range(0, self.length+1):
-            self.percentage = i / self.length
-            self.get_date()
-            print(str(self.convert_to_readable()) +
+        # exponent = ln(current-stop+e^3)-3
+        self.exponent = log(self.difference_in_dates / 365.2425 + e**3) - 3
+        for i in range(0, self.task_length+1):
+            self.percentage = i / self.task_length
+            self.get_in_progress_date()
+            print(str(self.get_readable_date()) +
                   " (" + str(self.percentage*100) + "%)")
             sleep(1)
+
+    def get_in_progress_date(self):
+        # date = current - (e^(exponent*p^3+3)-e^3)
+        self.in_progress_date = (self.start_date_decimal -
+                                 (e**(self.exponent*self.percentage**3+3) -
+                                  e**3))
+
+    def get_readable_date(self):
+        year = floor(self.in_progress_date)
+        total_days = self.number_of_days(year)
+        day = round((self.in_progress_date-year) * total_days)
+        start_point = date(year, 1, 1)
+        new_date = start_point + timedelta(days=day)
+        return new_date
 
     @staticmethod
     def number_of_days(year):
@@ -47,67 +90,7 @@ class Date(object):
         else:
             return 365
 
-    @staticmethod
-    def stop_date_input(datetype, datatype):
-        while True:
-            try:
-                value = (datatype(input("Enter the " + datetype +
-                         " you wish to stop in: ")))
-                if datetype == "month":
-                    assert value in Date.months
-            except (ValueError, AssertionError):
-                pass
-            else:
-                return value
-
-    @staticmethod
-    def get_number_of_seconds(tUnit, exponent):
-        while True:
-            try:
-                seconds = (int(input("Enter length of the task in " +
-                           tUnit + ": ")))
-                assert seconds % 1 == 0
-            except (ValueError, AssertionError):
-                print("Not a valid input.")
-            else:
-                return seconds * 60**exponent
-
-    def get_date(self):
-        self.dateInProgress = (self.current_date -
-                               (e**(self.exponent*self.percentage**3+3)-e**3))
-
-    def convert_to_readable(self):
-        year = floor(self.dateInProgress)
-        total_days = self.number_of_days(year)
-        day = round((self.dateInProgress-year) * total_days)
-        if day == 0:
-            day = 31
-            month = "December"
-            year -= 1
-            return month + " " + str(day) + ", " + str(year)
-
-        # Finds name of month
-        for i in sorted(list(Date.months.values()))[::-1]:
-            if total_days == 366 and i >= 59:
-                i += 1
-            if i >= day:
-                continue
-            else:
-                for j in Date.months:
-                    # Second condition avoids '(month) 0' outputs
-                    if Date.months[j] == i or Date.months[j] == i-1:
-                        month = j
-                        break
-                break
-        day -= Date.months[month]
-
-        # Fix for leap years
-        if total_days == 366 and Date.months[month] >= 59:
-            day -= 1
-
-        return month + " " + str(day) + ", " + str(year)
-
 
 if __name__ == "__main__":
-    userDate = Date()
-    userDate.progress()
+    user_task = Task()
+    user_task.progress()
